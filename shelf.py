7df29efedc2fcb66e69c041ded93460c1ba3bf73
Paul <paul.veltheim@gmail.com>
2026-04-13 16:58:19 +0300

    modified shelf functions to use db decorators

--- shelf.py
+++ shelf.py
@@ -3,67 +3,49 @@ import db
 import user
 from flask import redirect
 
+@db.query_db
 def get_shelves(username):
     """returns all bookshelves of a user"""
     
+    sql = """
+    SELECT s.name, COUNT(sb.book_id), s.description, s.id 
+    FROM shelves s
+    JOIN users u ON u.id = s.user_id
+    LEFT JOIN shelf_books sb ON s.id = sb.shelf_id
+    WHERE u.username = ?
+    GROUP BY s.name, s.description, s.id
+    """
 
-    try:
-	    sql = """
-        SELECT s.name, COUNT(sb.book_id), s.description, s.id 
-        FROM shelves s
-        JOIN users u ON u.id = s.user_id
-        LEFT JOIN shelf_books sb ON s.id = sb.shelf_id
-        WHERE u.username = ?
-        GROUP BY s.name, s.description, s.id
-        """
-	    shelves = db.query(sql, [username])
-    except Exception as e:
-        print(f"Database error in selecting shelves")
-        print(e)    
-        raise
+    return g.db_query(sql, [username])
 
-    return shelves
-
+@db.query_db
 def get_shelf(shelf_name, username):
     """returns a shelf based on db id"""
 
-    try:
-        sql = """
-        SELECT s.name, COUNT (sb.book_id), s.description
-        FROM shelves s
-        JOIN users u ON u.id = s.user_id
-        LEFT JOIN shelf_books sb ON s.id = sb.shelf_id 
-        WHERE s.name = ? AND u.username = ?
-        """
-        shelf = db.query(sql, [shelf_name, username])
-    except Exception as e:
-        print("Database error in fetching shelf")
-        print(e)
-        raise
+    sql = """
+    SELECT s.name, COUNT (sb.book_id), s.description
+    FROM shelves s
+    JOIN users u ON u.id = s.user_id
+    LEFT JOIN shelf_books sb ON s.id = sb.shelf_id 
+    WHERE s.name = ? AND u.username = ?
+    """
+    
+    return g.db_query(sql, [shelf_name, username])
 
-    return shelf
-
+@db.modify_db
 def create_shelf(user_id, name, description, public):
     """creates new bookshelf"""
     
-    try:
-	    sql = "INSERT INTO shelves (user_id, name, description, public) VALUES (?, ?, ?, ?)"
-	    db.execute(sql, [user_id, name, description, public])
-    except Exception as e:
-        print(e)
-        print("Database error in creating new shelf")
-        raise
-
+    sql = "INSERT INTO shelves (user_id, name, description, public) VALUES (?, ?, ?, ?)"
+    g.db_execute(sql, [user_id, name, description, public])
+    
+@db.modify_db
 def delete_shelf(shelf_id):
     """deletes a bookshelf based on db id"""
 
-    try:
-        sql = "DELETE FROM shelves WHERE id = ?"
-        db.execute(sql, [shelf_id])
-    except Exception as e:
-        print(e)
-        print("Database error in deleting shelf")
-        raise
+    sql = "DELETE FROM shelves WHERE id = ?"
+    g.db_execute(sql, [shelf_id])
+    
 
 
 
